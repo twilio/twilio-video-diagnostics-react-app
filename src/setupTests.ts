76127bab33e48a1f14dd161f0d8c3850a1c267a6
Olivia Pyskoty <opyskoty@twilio.com>
2021-05-27 11:13:57 -0400

    update testing packages

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import '@testing-library/jest-dom';
+
+Enzyme.configure({ adapter: new Adapter() });
