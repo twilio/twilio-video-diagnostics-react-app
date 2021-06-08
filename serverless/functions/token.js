f6efaaa15f77a9478af2361a23d2f44313ac2e11
Olivia Pyskoty <opyskoty@twilio.com>
2021-06-08 17:41:22 -0400

    add token endpoint

---
+++ serverless/functions/token.js
@@ -0,0 +1,19 @@
+exports.handler = function (context, event, callback) {
+  const verifyExpiry = require(Runtime.getAssets()['/verify_expiry.js'].path);
+  verifyExpiry.handler(context, event, callback);
+
+  const AccessToken = Twilio.jwt.AccessToken;
+  const VideoGrant = AccessToken.VideoGrant;
+
+  const videoGrant = new VideoGrant({
+    room: event.room_name
+  });
+
+  const token = new AccessToken(context.ACCOUNT_SID, context.API_KEY, context.API_SECRET, {
+    ttl: 60,
+  });
+  token.addGrant(videoGrant);
+  token.identity = context.VIDEO_IDENTITY;
+
+  callback(null, { token: token.toJwt() });
+};
--- serverless/functions/turn-credentials.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function (context, event, callback) {
-    const verifyExpiry = require(Runtime.getAssets()['/verify_expiry.js'].path);
-    verifyExpiry.handler(context, event, callback);
-  
-    const client = context.getTwilioClient();
-    client.tokens.create({ ttl: 30 }).then((token) => callback(null, token));
-  };
