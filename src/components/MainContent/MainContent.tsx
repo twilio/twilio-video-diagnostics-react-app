ec45714948d532fb213a55e2933a5d4372393ae3
Tim Mendoza <tmendoza@twilio.com>
2021-06-17 16:33:44 -0600

    Remove unused theme variable

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,66 +1,65 @@
 import { ActivePane, useAppStateContext } from '../AppStateProvider/AppStateProvider';
 import { ArrowDown } from '../../icons/ArrowDown';
 import { ArrowUp } from '../../icons/ArrowUp';
-import { Button, createStyles, makeStyles, Theme, useTheme } from '@material-ui/core';
+import { Button, makeStyles, useTheme } from '@material-ui/core';
 import clsx from 'clsx';
 import { GetStarted } from '../panes/GetStarted/GetStarted';
 import { useEffect, useRef } from 'react';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    contentContainer: {
-      position: 'absolute',
-      top: '0',
-      left: '50%',
-      transform: 'translateX(-50%)',
+const useStyles = makeStyles({
+  contentContainer: {
+    position: 'absolute',
+    top: '0',
+    left: '50%',
+    transform: 'translateX(-50%)',
+  },
+  scrollContainer: {
+    transition: 'all 1s ease',
+    position: 'relative',
+    transform: 'translateY(50vh)',
+    '& .inactive': {
+      opacity: 0.2,
+      userSelect: 'none',
+      cursor: 'pointer',
     },
-    scrollContainer: {
-      transition: 'all 1s ease',
-      position: 'relative',
-      transform: 'translateY(50vh)',
-      '& .inactive': {
-        opacity: 0.2,
-        userSelect: 'none',
-        cursor: 'pointer',
+  },
+  hideAll: {
+    '& .inactive': {
+      opacity: 0,
+      visibility: 'hidden',
+    },
+  },
+  item: {
+    transition: 'all 1s ease',
+    padding: '3em 0',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 30,
+    right: 30,
+    display: 'flex',
+    flexDirection: 'column',
+    '& button': {
+      minWidth: 0,
+      padding: '0.6em 0.9em',
+      background: 'white',
+      '&:first-child': {
+        marginBottom: '1.5em',
       },
-    },
-    hideAll: {
-      '& .inactive': {
-        opacity: 0,
+      '&[disabled]': {
         visibility: 'hidden',
       },
     },
-    item: {
-      transition: 'all 1s ease',
-      padding: '3em 0',
-    },
-    buttonContainer: {
-      position: 'absolute',
-      bottom: 30,
-      right: 30,
-      display: 'flex',
-      flexDirection: 'column',
-      '& button': {
-        minWidth: 0,
-        padding: '0.6em 0.9em',
-        '&:first-child': {
-          marginBottom: '1.5em',
-        },
-        '&[disabled]': {
-          visibility: 'hidden',
-        },
-      },
-    },
-    brandSidebar: {
-      background: '#06033A',
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      right: 0,
-      left: 'calc(100% - 250px)',
-    },
-  })
-);
+  },
+  brandSidebar: {
+    background: '#06033A',
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 'calc(100% - 250px)',
+  },
+});
 
 export function Item({
   children,
