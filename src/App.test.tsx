28c540b5ffdc69edf0a6f4a7a51b215f30d763ff
Tim Mendoza <tmendoza@twilio.com>
2021-06-16 21:35:28 -0600

    Remove placeholder App.test.tsx

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders app", () => {
-  const { getByText } = render(<App />);
-  const appText = getByText(/Twilio Video Diagnostics App/);
-  expect(appText).toBeInTheDocument();
-});
