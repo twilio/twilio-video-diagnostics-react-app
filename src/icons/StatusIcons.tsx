36a1a0be8b1d01e809afdcc055e2cbf6ac78c1e7
Tim Mendoza <tmendoza@twilio.com>
2021-07-08 17:00:48 -0600

    Add success status icon

---
+++ src/icons/StatusIcons.tsx
@@ -0,0 +1,6 @@
+export const SuccessStatus = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="20" height="20" fill="white" fill-opacity="0.01" />
+    <circle cx="10" cy="10" r="6" fill="#14B053" />
+  </svg>
+);
