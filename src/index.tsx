4c85ad1c1879de2b47be559ddbd451c3dda9edd0
Tim Mendoza <tmendoza@twilio.com>
2021-07-08 16:59:27 -0600

    Remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import theme from './theme';
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
