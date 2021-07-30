8fe2c2e1f08f4918c6d9ca2a38998653b58ae60e
Olivia Pyskoty <opyskoty@twilio.com>
2021-07-30 14:26:21 -0400

    move loading screen to connectivity

--- src/components/panes/Connectivity/Connectivity.test.tsx
+++ src/components/panes/Connectivity/Connectivity.test.tsx
@@ -1,7 +1,7 @@
+import { shallow } from 'enzyme';
 import { Connectivity } from './Connectivity';
 import { ConnectionFailed } from './ConnectionFailed/ConnectionFailed';
 import { ConnectionSuccess } from './ConnectionSuccess/ConnectionSuccess';
-import { shallow } from 'enzyme';
 import { useAppStateContext } from '../../AppStateProvider/AppStateProvider';
 
 jest.mock('../../AppStateProvider/AppStateProvider');
@@ -9,6 +9,39 @@ jest.mock('../../AppStateProvider/AppStateProvider');
 const mockUseAppStateContext = useAppStateContext as jest.Mock<any>;
 
 describe('the Connectivity component', () => {
+  it('should show the loading screen if preflight test is running', () => {
+    mockUseAppStateContext.mockImplementationOnce(() => ({
+      state: {
+        activePane: 4,
+        twilioStatus: 'operational',
+        preflightTest: {
+          progress: 'connected',
+          error: null,
+        },
+      },
+    }));
+
+    const wrapper = shallow(<Connectivity />);
+
+    expect(wrapper.text().includes('Hang Tight!')).toBe(true);
+  });
+
+  it('should hide the loading screen if preflight test has completed or thrown an error', () => {
+    mockUseAppStateContext.mockImplementationOnce(() => ({
+      state: {
+        activePane: 4,
+        twilioStatus: 'operational',
+        preflightTest: {
+          progress: null,
+          report: 'mockReport',
+          error: null,
+        },
+      },
+    }));
+    const wrapper = shallow(<Connectivity />);
+    expect(wrapper.text().includes('Hang Tight!')).toBe(false);
+  });
+
   it('should render ConnectionFailed component if connection has failed', () => {
     mockUseAppStateContext.mockImplementationOnce(() => ({
       state: {
@@ -16,6 +49,8 @@ describe('the Connectivity component', () => {
         twilioStatus: 'operational',
         preflightTest: {
           progress: 'connected',
+          report: null,
+          error: 'mockError',
         },
       },
     }));
@@ -30,6 +65,7 @@ describe('the Connectivity component', () => {
         twilioStatus: 'operational',
         preflightTest: {
           progress: null,
+          report: 'mockReport',
         },
       },
     }));
--- src/components/panes/Connectivity/Connectivity.tsx
+++ src/components/panes/Connectivity/Connectivity.tsx
@@ -1,20 +1,54 @@
-import { useAppStateContext } from '../../AppStateProvider/AppStateProvider';
-import { ConnectionSuccess } from './ConnectionSuccess/ConnectionSuccess';
+import { Container, Grid, Typography, makeStyles } from '@material-ui/core';
 import { ConnectionFailed } from './ConnectionFailed/ConnectionFailed';
+import { ConnectionSuccess } from './ConnectionSuccess/ConnectionSuccess';
+import { Loading } from '../../../icons/Loading';
+import { ActivePane, useAppStateContext } from '../../AppStateProvider/AppStateProvider';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  textBox: {
+    width: '340px',
+    height: '48px',
+    textAlign: 'center',
+  },
+});
 
 export function Connectivity() {
   const { state } = useAppStateContext();
+  const classes = useStyles();
 
   const twilioServicesStatus = state.twilioStatus === 'operational' ? 'Up' : 'Down';
   const signalingGateway = state.preflightTest.progress === 'connected' ? 'Unreachable' : 'Reachable';
   const turnServers = state.preflightTest.progress === 'mediaStarted' ? 'Unreachable' : 'Reachable';
 
-  // If any of the above are Unreachable or Down, connection has failed:
-  const connectionFailed = twilioServicesStatus !== 'Up' || state.preflightTest.progress;
+  const showLoading =
+    state.activePane === ActivePane.Connectivity &&
+    state.preflightTest.progress !== null &&
+    state.preflightTest.error === null;
+
+  const connectionFailed =
+    twilioServicesStatus !== 'Up' || (state.preflightTest.progress && state.preflightTest.error !== null);
 
   return (
+    // If preflight test hasn't completed, display loading screen otherwise, display connectivity results:
     <>
-      {connectionFailed ? (
+      {showLoading ? (
+        <Container>
+          <Grid container className={classes.container}>
+            <Typography variant="h1" gutterBottom>
+              Hang Tight!
+            </Typography>
+            <Typography variant="body1" gutterBottom className={classes.textBox}>
+              We're just finishing up your personalized results and creating your report.
+            </Typography>
+            <Loading />
+          </Grid>
+        </Container>
+      ) : connectionFailed ? (
         <ConnectionFailed
           serviceStatus={twilioServicesStatus}
           signalingGateway={signalingGateway}
