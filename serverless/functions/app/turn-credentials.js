95c20ecf432621786a3a0c7a4c3fcdcb33edb1fc
Olivia Pyskoty <opyskoty@twilio.com>
2021-08-18 16:45:55 -0400

    remove new line

--- serverless/functions/app/turn-credentials.js
+++ serverless/functions/app/turn-credentials.js
@@ -5,4 +5,3 @@ exports.handler = function (context, event, callback) {
     const client = context.getTwilioClient();
     client.tokens.create({ ttl: 30 }).then((token) => callback(null, token));
   };
-  
