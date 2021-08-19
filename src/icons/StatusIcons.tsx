15f8f6553611122ab0811a8350f89c76054d0e0b
Olivia Pyskoty <opyskoty@twilio.com>
2021-08-19 17:55:27 -0400

    update error status icon

--- src/icons/StatusIcons.tsx
+++ src/icons/StatusIcons.tsx
@@ -7,7 +7,7 @@ export const SuccessStatus = () => (
 
 export const ErrorStatus = () => (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="20" height="20" fill="white" fillOpacity="0.01" />
-    <circle cx="10" cy="10" r="6" fill="#D61F1F" />
+    <path d="M0 0H20V20H0V0Z" fill="white" fillOpacity="0.01" />
+    <rect x="10.0713" y="3" width="10" height="10" transform="rotate(45 10.0713 3)" fill="#D61F1F" />
   </svg>
 );
