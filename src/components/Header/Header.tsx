f48627d153576ac7a4e666ca8c3e475ae877f4b8
Olivia Pyskoty <opyskoty@twilio.com>
2021-08-26 18:10:42 -0400

    change to Device and Software Setup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -73,7 +73,7 @@ export default function Header() {
     <div className={classes.header}>
       <Container>
         <Grid container alignItems="center" justifyContent="space-between" style={{ height: '100%' }}>
-          <HeaderItem pane={ActivePane.DeviceCheck} label="Device & Network Setup" />
+          <HeaderItem pane={ActivePane.DeviceCheck} label="Device & Software Setup" />
           <HeaderItem pane={ActivePane.Connectivity} label="Connectivity" />
           <HeaderItem pane={ActivePane.Quality} label="Quality & Performance" />
           <HeaderItem pane={ActivePane.Results} label="Get Results" />
