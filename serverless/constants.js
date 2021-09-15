450cd8853a86f48c9000f92e2c2260ca2d0f157c
johannesplanis <jan.pluta.jr@gmail.com>
2021-09-15 16:22:42 +0200

    Change branding to Therapify

--- serverless/constants.js
+++ serverless/constants.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  SERVICE_NAME: 'rtc-diagnostics-video',
+  SERVICE_NAME: 'therapify-video-diagnostics',
   API_KEY_NAME: 'RTC Video Diagnostics Key',
   VIDEO_IDENTITY: process.env.VIDEO_IDENTITY || 'RTC_Video_Diagnostics_Test_Identity',
 };
