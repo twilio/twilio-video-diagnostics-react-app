4c6e370c5414e955e8b319b044e1a154860e98bd
olipyskoty <77076398+olipyskoty@users.noreply.github.com>
2022-02-23 13:02:47 -0800

    Video 8571 mobile audio test (#31)

--- src/theme.ts
+++ src/theme.ts
@@ -62,6 +62,10 @@ export default createTheme({
           width: '100vw',
           maxWidth: '610px',
         },
+        [defaultTheme.breakpoints.up(BREAKPOINTS.values.md)]: {
+          paddingLeft: '50px',
+          paddingRight: '50px',
+        },
       },
     },
     MuiButton: {
