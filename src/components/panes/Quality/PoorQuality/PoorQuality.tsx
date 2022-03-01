ced63bc74add0a673bc609b70f8871f6323f81b7
Olivia Pyskoty <opyskoty@twilio.com>
2022-03-01 13:27:30 -0800

    add mobile styling for quality panes

--- src/components/panes/Quality/PoorQuality/PoorQuality.tsx
+++ src/components/panes/Quality/PoorQuality/PoorQuality.tsx
@@ -1,40 +1,71 @@
-import { Container, Grid, Typography, Button, Paper, makeStyles } from '@material-ui/core';
+import { Container, Grid, Typography, Button, Paper, makeStyles, Theme, createStyles } from '@material-ui/core';
 import { ErrorIcon } from '../../../../icons/ErrorIcon';
 import { QualityScore } from '../Quality';
 import { useAppStateContext } from '../../../AppStateProvider/AppStateProvider';
 import { ViewIcon } from '../../../../icons/ViewIcon';
 import { WarningIcon } from '../../../../icons/WarningIcon';
 
-const useStyles = makeStyles({
-  modal: {
-    width: '6000px',
-  },
-  paper: {
-    padding: '1.5em',
-    borderRadius: '8px',
-    '& li': {
-      '&:not(:last-child)': {
-        marginBottom: '1.5em',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    header: {
+      float: 'left',
+      [theme.breakpoints.down('md')]: {
+        float: 'initial',
       },
     },
-  },
-  paperContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  errorIcon: {
-    position: 'absolute',
-    right: 'calc(100% + 18px)',
-  },
-  viewButton: {
-    marginTop: '2em',
-    '& svg': {
+    heading: {
       position: 'relative',
-      left: '-5px',
     },
-  },
-});
+    errorIcon: {
+      position: 'absolute',
+      right: 'calc(100% + 15px)',
+      [theme.breakpoints.down('sm')]: {
+        position: 'relative',
+        right: '0',
+        marginBottom: '0.5em',
+      },
+    },
+    modal: {
+      width: '6000px',
+    },
+    paper: {
+      padding: '1.2em',
+      maxWidth: '400px',
+      borderRadius: '8px',
+      '& li': {
+        '&:not(:last-child)': {
+          marginBottom: '1.5em',
+        },
+      },
+    },
+    paperContainer: {
+      float: 'right',
+      marginRight: '1em',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      [theme.breakpoints.down('md')]: {
+        float: 'initial',
+        justifyContent: 'center',
+        margin: '0 0 2.5em 0',
+      },
+    },
+    viewButton: {
+      marginTop: '2em',
+      '& svg': {
+        position: 'relative',
+        left: '-5px',
+      },
+    },
+    confirmationButtons: {
+      clear: 'left',
+      [theme.breakpoints.down('md')]: {
+        clear: 'initial',
+        marginBottom: '2em',
+      },
+    },
+  })
+);
 
 interface PoorQualityProps {
   quality: QualityScore | undefined;
@@ -47,9 +78,9 @@ export function PoorQuality({ quality, openModal }: PoorQualityProps) {
 
   return (
     <Container>
-      <Grid container alignItems="center" justifyContent="space-between">
-        <Grid item md={5}>
-          <Typography variant="h1" gutterBottom style={{ position: 'relative' }}>
+      <div>
+        <Grid item lg={5} className={classes.header}>
+          <Typography variant="h1" gutterBottom className={classes.heading}>
             <div className={classes.errorIcon}>
               {quality === QualityScore.Suboptimal ? <WarningIcon /> : <ErrorIcon />}
             </div>
@@ -61,20 +92,9 @@ export function PoorQuality({ quality, openModal }: PoorQualityProps) {
             <strong>{quality === QualityScore.Suboptimal ? 'suboptimal' : 'degraded'} </strong>
             and overall performance {quality === QualityScore.Suboptimal ? 'could be better' : 'will be poor'}.
           </Typography>
-
-          <Typography variant="body1" gutterBottom>
-            <strong>Did you try all of the tips to improve quality?</strong>
-          </Typography>
-
-          <Button variant="contained" color="primary" onClick={nextPane} style={{ marginRight: '1.5em' }}>
-            Yes
-          </Button>
-          <Button color="primary" onClick={nextPane}>
-            Skip for now
-          </Button>
         </Grid>
 
-        <Grid item md={5} className={classes.paperContainer}>
+        <Grid item lg={5} className={classes.paperContainer}>
           <Paper className={classes.paper}>
             <Typography variant="body1">
               <strong>Tips to improve quality and performance:</strong>
@@ -103,7 +123,20 @@ export function PoorQuality({ quality, openModal }: PoorQualityProps) {
             View detailed quality information
           </Button>
         </Grid>
-      </Grid>
+
+        <Grid item lg={5} className={classes.confirmationButtons}>
+          <Typography variant="body1" gutterBottom>
+            <strong>Did you try all of the tips to improve quality?</strong>
+          </Typography>
+
+          <Button variant="contained" color="primary" onClick={nextPane} style={{ marginRight: '1.5em' }}>
+            Yes
+          </Button>
+          <Button color="primary" onClick={nextPane}>
+            Skip for now
+          </Button>
+        </Grid>
+      </div>
     </Container>
   );
 }
